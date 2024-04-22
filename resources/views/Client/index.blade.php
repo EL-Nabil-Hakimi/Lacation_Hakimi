ea70881fc1ff1d8550f4d105d22a7e6ed481edc7
EL-Nabil-Hakimi <nabilelhakimi2023@gmail.com>
2024-04-22 15:04:09 +0100

    Update car mark and model select elements in index.blade.php

--- resources/views/Client/index.blade.php
+++ resources/views/Client/index.blade.php
@@ -47,7 +47,7 @@
                   <h2>Make your trip</h2>
                   <div class="form-group">
                     <label for="car_mark" class="label">Mark</label>
-                    <select name="marque_id"  style=" color: white !important" class="form-control" id="car_mark" required>
+                    <select name="marque_id"  style=" color: white !important" class="form-control" id="car_mark" >
 
                       <option style="background-color: #1089ff !important; color: white" value="" selected disabled>Ferrari ...</option>
 
@@ -61,7 +61,7 @@
 
                 <div class="form-group">
                     <label for="car_model" class="label">Model</label>
-                    <select name="model_id" style=" color: white !important" class="form-control" id="car_model" required>
+                    <select name="model_id" style=" color: white !important" class="form-control" id="car_model" >
 
                     </select>
                 </div>
